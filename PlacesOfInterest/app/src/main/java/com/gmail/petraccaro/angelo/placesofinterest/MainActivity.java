fa2fac4aaff2e0a0cf8891f0d2425e0fe53b922c
Antonio04-cmyk <antoniorapuano323@gmail.com>
2020-12-03 07:58:52 +0100

    Added user navigation drawer. It can be accessed by lateral swipe on the main acivity

--- PlacesOfInterest/app/src/main/java/com/gmail/petraccaro/angelo/placesofinterest/MainActivity.java
+++ PlacesOfInterest/app/src/main/java/com/gmail/petraccaro/angelo/placesofinterest/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_user_nav);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
